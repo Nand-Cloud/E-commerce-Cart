bbfb72aee9942a8946f1f5fca1c170a3707082ac
Nand Kishor Singh <nand49a@gmail.com>
2025-02-18 16:10:17 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png' 
+import imageProduct2 from './assets/images/2.png' 
+import imageProduct3 from './assets/images/3.png' 
+import imageProduct4 from './assets/images/4.png' 
+import imageProduct5 from './assets/images/5.png' 
+import imageProduct6 from './assets/images/6.png' 
+import imageProduct7 from './assets/images/7.png' 
+import imageProduct8 from './assets/images/8.png' 
+export const products = [
+    {
+        id: 1,
+        name: 'Godrej Window AC',
+        price: 28599,
+        image: imageProduct1,
+        description: 'Model: WFC 18UTC3-WWB',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Daikin 0.8 ton Split AC',
+        price: 26490,
+        image: imageProduct2,
+        description: 'Model: FTL28U',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'General 1.5 ton AC',
+        price: 65999,
+        image: imageProduct3,
+        description: 'Model: ASGG18CGTB',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Daikin Casstte AC',
+        price: 350090,
+        image: imageProduct4,
+        description: 'Model: SDG3490758N',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Voltas Split AC',
+        price: 25100,
+        image: imageProduct5,
+        description: 'Model: FGHJ98765T',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Mitsubishi Heavy Hot and Cold Inverter Split AC',
+        price: 184700,
+        image: imageProduct6,
+        description: 'Model: YGDB4567U',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Godrej 1.5 Ton 3 Star Split Inverter AC',
+        price: 37500,
+        image: imageProduct7,
+        description: 'Model: WERV0245I',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Parasonic Smart AC',
+        price: 36780,
+        image: imageProduct8,
+        description: '',
+        slug: 'Model: VBNT7654D'
+    },
+];
